16580daf2bd93564b56f3ed9294bcbd81d180fd3
NicolasTablon <125476767+NicolasTablon@users.noreply.github.com>
2023-06-12 15:56:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,27 @@ def get_director(director: str):
 
     return [director, retorno_director, resp]
 
+
+from flask import Flask, jsonify, request
+
+ 
+ @app.route('/recomendacion', methods=['GET'])
+def recomendacion():
+    titulo = request.args.get('titulo')  # Obtener el parámetro 'titulo' de la solicitud GET
+    
+    # Filtrar las películas similares basadas en el título proporcionado
+    pelicula_actual = df[df['title'] == titulo]
+    puntajes_similares = df[df['vote_average'] >= pelicula_actual['vote_average'].values[0]]
+    peliculas_similares = puntajes_similares.sort_values(by='vote_average', ascending=False)['title'].tolist()
+    
+    # Devolver una lista de 5 películas recomendadas en formato JSON
+    recomendaciones = peliculas_similares[:5]
+    return jsonify(recomendaciones)
+
+if __name__ == '__main__':
+    app.run()
+
+    return recommended_movies
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola"
