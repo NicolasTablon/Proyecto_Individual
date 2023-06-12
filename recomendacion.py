7b0c77a266a5811021f5f1a98475f7920ac6a467
NicolasTablon <125476767+NicolasTablon@users.noreply.github.com>
2023-06-12 15:39:10 -0300

    Create recomendacion.py

---
+++ recomendacion.py
@@ -0,0 +1,23 @@
+def recomendacion(titulo):
+    # Cargar el conjunto de datos de películas
+    df_movies = pd.read_csv('ruta/al/archivo/csv/con/puntuaciones_de_peliculas.csv')
+
+    # Normalizar los títulos de las películas en la columna "title"
+    df_movies['title'] = df_movies['title'].str.lower().str.strip()
+
+    # Filtrar por el título de la película ingresada
+    selected_movie = df_movies[df_movies['title'] == titulo.lower().strip()]
+
+    if selected_movie.empty:
+        return "La película no se encuentra en la base de datos."
+
+    # Calcular la similitud de puntuación entre la película ingresada y el resto de películas
+    similarity_scores = df_movies[['title', 'vote_average']].corrwith(selected_movie['vote_average'])
+
+    # Ordenar las películas según el score de similitud en orden descendente
+    similar_movies = similarity_scores.sort_values(ascending=False)
+
+    # Obtener los nombres de las películas recomendadas (excluyendo la película ingresada)
+    recommended_movies = similar_movies.index[1:6].tolist()
+
+    return recommended_movies
